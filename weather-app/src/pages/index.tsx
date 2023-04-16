49d6c5e07bfb68f10b5c00bae9a9d6743dbbdb59
Vladislav Oliinyk <catdev.dev@gmail.com>
2023-04-16 03:28:07 +0300

    test

--- weather-app/src/pages/index.tsx
+++ weather-app/src/pages/index.tsx
@@ -11,7 +11,7 @@ export const Routing = () => {
       <Route
         element={
           <Layout
-            headerContent={<div>test 123 123</div>}
+            headerContent={<div>test 123 123 123</div>}
             sidebarNavigationContent={<MainNavigationSidebar />}
           />
         }
