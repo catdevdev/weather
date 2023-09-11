b5b322df3104cc2787bff66949628f9c37d89030
Vladislav Oliinyk <catprogrammer.vlad@gmail.com>
2023-09-11 21:42:35 +0300

    333

--- weather-app/src/pages/Overview/index.tsx
+++ weather-app/src/pages/Overview/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useEffect, useRef } from 'react'
 import { Outlet, useParams, useSearchParams } from 'react-router-dom'
 import { io } from 'socket.io-client'
--- weather-app/src/widgets/WeatherstationsMap/index.tsx
+++ weather-app/src/widgets/WeatherstationsMap/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useEffect } from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { useNavigate } from 'react-router-dom'
